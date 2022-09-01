3e1d2d32be0604d2cebb6d071958bcabbf24341b
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2022-08-31 23:03:11 -0500

    activity lifecycle methods calls

--- FirstAppDemo/app/src/main/java/mx/uv/sbc/formappdemo/MainActivity.java
+++ FirstAppDemo/app/src/main/java/mx/uv/sbc/formappdemo/MainActivity.java
@@ -2,16 +2,19 @@ package mx.uv.sbc.formappdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    private static String TAG = "MyFirstApp";
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate (@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView (R.layout.activity_main);
 
@@ -25,6 +28,47 @@ public class MainActivity extends Activity {
         });
 
         Button btnClear = findViewById (R.id.btnClear);
+    }
 
+    @Override
+    protected void onStart () {
+        super.onStart ();
+        Log.d (TAG, "OnStart");
+    }
+
+    @Override
+    protected void onResume () {
+        super.onResume ();
+        Log.d (TAG, "OnResume");
+    }
+
+    @Override
+    protected void onPause () {
+        super.onPause ();
+        Log.d (TAG, "OnPause");
+    }
+
+    @Override
+    protected void onStop () {
+        super.onStop ();
+        Log.d (TAG, "OnStop");
+    }
+
+    @Override
+    protected void onDestroy () {
+        super.onDestroy ();
+        Log.d (TAG, "OnDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState (@NonNull Bundle outState) {
+        super.onSaveInstanceState (outState);
+        Log.d (TAG, "OnSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d (TAG, "OnRestoreInstanceState");
     }
 }
