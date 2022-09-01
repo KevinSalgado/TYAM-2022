c3581d5b4bee920df9e786241475b89c414c58b0
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2022-08-31 23:10:20 -0500

    saving form data

--- FirstAppDemo/app/src/main/java/mx/uv/sbc/formappdemo/MainActivity.java
+++ FirstAppDemo/app/src/main/java/mx/uv/sbc/formappdemo/MainActivity.java
@@ -2,22 +2,29 @@ package mx.uv.sbc.formappdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    private static String NAME_KEY = "name";
+    private static String LAST_NAME_KEY = "lastname";
+    private static String PHONE_KEY = "phone";
+    private static String TAG = "MyFirstApp";
+    private EditText edtName, edtLastname, edtPhone;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView (R.layout.activity_main);
 
-        EditText edtName = findViewById (R.id.edtName);
-        EditText edtLastname = findViewById (R.id.edtLastname);
-        EditText edtPhone = findViewById (R.id.edtPhone);
+        edtName = findViewById (R.id.edtName);
+        edtLastname = findViewById (R.id.edtLastname);
+        edtPhone = findViewById (R.id.edtPhone);
 
         Button btnSend = findViewById (R.id.btnSend);
         btnSend.setOnClickListener (view -> {
@@ -25,6 +32,60 @@ public class MainActivity extends Activity {
         });
 
         Button btnClear = findViewById (R.id.btnClear);
+    }
 
+    @Override
+    protected void onStart () {
+        super.onStart ();
+        Log.d (TAG, "OnStart");
+    }
+
+    @Override
+    protected void onResume () {
+        super.onResume ();
+        Log.d (TAG, "OnResume");
+    }
+
+    @Override
+    protected void onPause () {
+        super.onPause ();
+        Log.d (TAG, "OnPause");
+    }
+
+    @Override
+    protected void onStop () {
+        super.onStop ();
+        Log.d (TAG, "OnStop");
+    }
+
+    @Override
+    protected void onDestroy () {
+        super.onDestroy ();
+        Log.d (TAG, "OnDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState (@NonNull Bundle outState) {
+        super.onSaveInstanceState (outState);
+        Log.d (TAG, "OnSaveInstanceState");
+
+        outState.putString (NAME_KEY, edtName.getText().toString ());
+        outState.putString (LAST_NAME_KEY, edtLastname.getText().toString ());
+        outState.putString (PHONE_KEY, edtPhone.getText().toString ());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d (TAG, "OnRestoreInstanceState");
+
+        String name = savedInstanceState.getString (NAME_KEY);
+        edtName.setText (name);
+
+        String lastname = savedInstanceState.getString (LAST_NAME_KEY);
+        edtLastname.setText (lastname);
+
+        String phone = savedInstanceState.getString (PHONE_KEY);
+        edtPhone.setText (phone);
     }
 }
